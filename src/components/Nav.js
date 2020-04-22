cee56a1e5d30f410203b0944aa0213b58e787d40
Jessica <yutsuc@gmail.com>
2020-04-22 11:26:00 -0700

    feat: Update Nav to use Material UI buttons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Button } from "@material-ui/core"
 
 class Nav extends React.Component {
     render = () => {
         return (
             <div className="nav">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/add">New Question</NavLink>
-                <NavLink to="/leaderboard">Leaderboard</NavLink>
+                <Button component={NavLink} to="/">Home</Button>
+                <Button component={NavLink} to="/add">New Question</Button>
+                <Button component={NavLink} to="/leaderboard">Leaderboard</Button>
             </div>
         )
     }
