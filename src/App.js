6cc08c2a979cf265c7112142116d1f040bc9d2b7
Jessica <yutsuc@gmail.com>
2020-04-21 21:14:03 -0700

    feat: Link react and redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import { connect } from "react-redux";
+import { handleInitialData } from "./actions/shared";
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+class App extends React.Component {
+    componentDidMount = () => {
+        this.props.dispatch(handleInitialData());
+    }
+
+    render = () => {
+        return (
+            <div className="App">
+                App
+            </div>
+        );
+    }
 }
 
-export default App;
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
+import reducer from "./reducers";
+import middleware from "./middleware";
 
+const store = createStore(reducer, middleware);
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
