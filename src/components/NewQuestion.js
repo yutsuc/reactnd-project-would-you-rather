50cd292e425d7b32918f33d324acd15b3752c3b9
Jessica <yutsuc@gmail.com>
2020-04-22 11:26:47 -0700

    feat: Update New Question UI

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Button, Card, CardContent, CardActions, TextField } from "@material-ui/core";
 
 class NewQuestion extends React.Component {
     render = () => {
         return (
             <div>
-                NewQuestion
+                <h3>Create New Question</h3>
+                <Card className="new-question">
+                    <CardContent>
+                        <form className="question-input">
+                            <TextField className="fullwidth" label="test1" multiline />
+                            <div className="question-divider">----------------- or -----------------</div>
+                            <TextField className="fullwidth" label="test2" multiline />
+                        </form>
+                    </CardContent>
+                    <CardActions>
+                        <Button className="fullwidth" variant="outlined" color="primary">Sumbit</Button>
+                    </CardActions>
+                </Card>
             </div>
         );
     }
