f09fceb6de7f9ba7e87cc44e7aaaf868f68b992e
Jessica <yutsuc@gmail.com>
2020-04-23 01:46:15 -0700

    feat: Add logged in user name on nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Button } from "@material-ui/core"
+import { Button } from "@material-ui/core";
+import { connect } from "react-redux";
 
 class Nav extends React.Component {
     render = () => {
         return (
             <div className="nav">
-                <Button component={NavLink} to="/">Home</Button>
-                <Button component={NavLink} to="/add">New Question</Button>
-                <Button component={NavLink} to="/leaderboard">Leaderboard</Button>
+                <div>
+                    <Button component={NavLink} to="/">Home</Button>
+                    <Button component={NavLink} to="/add">New Question</Button>
+                    <Button component={NavLink} to="/leaderboard">Leaderboard</Button>
+                </div>
+                <div className="spacer"></div>
+                <div>
+                    <span>Hello, <strong>{this.props.userName}</strong></span>
+                    <Button >Log Out</Button>
+                </div>
             </div>
         )
     }
 }
 
-export default Nav;
+const mapStateToProps = ({ authedUser, users }) => {
+    return {
+        userName: authedUser === null ? "" : users[authedUser].name,
+    };
+}
+export default connect(mapStateToProps)(Nav);
